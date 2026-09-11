9f4b90cf8c9dfacd2b5fdb38287a01df7ea58961
Charles Beynon <etothepiipower@gmail.com>
2026-09-10 21:16:42 -0400

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.py
@@ -0,0 +1,17 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals = sorted(intervals, key=lambda i: i.start)
+        for idx, interval in enumerate(intervals):
+            if idx == len(intervals) - 1:
+                return True
+            if interval.end > intervals[idx + 1].start:
+                return False
+        return True
